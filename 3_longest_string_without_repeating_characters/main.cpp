692fa1924c00c70fffa8021ddd1428d8b0f5277f
mcakay <melihcakay@gmail.com>
2023-03-30 19:15:55 +0300

    longest_substr completed

--- 3_longest_string_without_repeating_characters/main.cpp
+++ 3_longest_string_without_repeating_characters/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-		(void)s;
-        return (0);
+		map<int, string> m;
+		string addString = "";
+		int addNumber = 0;
+		if (s.length() == 0)
+			return 0;
+		int j = 0;
+		for (size_t i = 0; i < s.length(); i++)
+		{
+			if (addString.find(s[i]) == string::npos)
+				addString += s[i];
+			else
+			{
+				m.insert(pair<int, string>(addNumber, addString));
+				i = j;
+				j++;
+				addNumber = 0;
+				addString = "";
+				continue;
+			}
+			addNumber++;
+		}
+		m.insert(pair<int, string>(addNumber, addString));
+		map<int, string>::iterator it = m.end();
+		it--;
+        return (it->first);
     }
 };
 
